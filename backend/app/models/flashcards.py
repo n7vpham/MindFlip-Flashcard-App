068b46e4f80b1cff58e18974b1485fa614074f87
isreednc <isaacreednc99@gmail.com>
2025-03-26 21:22:19 -0400

    Add user model functions

--- backend/app/models/flashcards.py
+++ backend/app/models/flashcards.py
@@ -1,7 +1,66 @@
 from .. import db
+from bson import json_util
+from bson.objectid import ObjectId
 
-class Flashcard:
-    pass
+collection = db['flashcards']
+
+def save_set(json):
+    try:
+        study_set = json_util.loads(json)
+        set_id = collection.insert_one(study_set)
+        return set_id 
+    except Exception as e:
+        print(e)
+        return None
+
+def save_flashcard(set_id, flashcard):
+    try:
+        result = collection.update_one({"_id": ObjectId(set_id)}, {"$push": {"flashcards": flashcard}})
+        return result.modified_count > 0
+    except Exception as e:
+        print(e)
+        return None
+
+def get_flashcards(set_id):
+    try:
+        study_set = collection.find_one({"_id", ObjectId(set_id)}, {"flashcards", 1})
+        if study_set and 'flashcards' in study_set:
+            return study_set['flashcards'] 
+        else:
+            return []
+    except Exception as e:
+        print(e)
+        return None
+
+def get_flashcard(user_id):
+    try:
+        user = collection.find_one({"_id": user_id})
+        return user
+    except Exception as e:
+        print(e)
+        return None
+
+def update_user(user_id, json):
+    try:
+        new_user = json_util.loads(json)
+        user = collection.replace_one({"_id": ObjectId(user_id)}, new_user)
+        if user is None:
+            raise
+        else:
+            return user.modified_count > 0
+    except Exception as e:
+        print(e)
+        return False
+
+def delete_user(user_id):
+    try:
+        user = collection.delete_one({"_id": user_id})
+        return user.deleted_count > 0
+    except Exception as e:
+        print(e)
+        return False
+
+
 
 # TODO: recreate this as flask model class above
 flashcards_schema = {
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,25 +1,70 @@
 from .. import db
+from bson import json_util
+from bson.objectid import ObjectId
 
-class User:
-    pass
+collection = db['users']
 
-# TODO: recreate this as flask model class above
-user_schema = {
-    "validator": {
-        "$jsonSchema": {
-            "bsonType": "object",
-            "required": ["firstName", "lastName", "username", "password", "userID", "studySets"],
-            "properties": {
-                "firstName": {"bsonType": "string"},
-                "lastName": {"bsonType": "string"},
-                "username": {"bsonType": "string"},
-                "password": {"bsonType": "string"},
-                "userID": {"bsonType": "string"},
-                "studySets": {
-                    "bsonType": "array",
-                    "items": {"bsonType": "string"}
-                }
-            }
-        }
-    }
-}
+def save_user(json):
+    try:
+        user = json_util.loads(json)
+        user_id = collection.insert_one(user)
+        return user_id 
+    except Exception as e:
+        print(e)
+        return None
+
+def get_users():
+    try:
+        cursor = collection.find()
+        return list(cursor)
+    except Exception as e:
+        print(e)
+        return None
+
+def get_user_by_id(user_id):
+    try:
+        user = collection.find_one({"_id": user_id})
+        return user
+    except Exception as e:
+        print(e)
+        return None
+
+def update_user(user_id, json):
+    try:
+        new_user = json_util.loads(json)
+        user = collection.replace_one({"_id": ObjectId(user_id)}, new_user)
+        if user is None:
+            raise
+        else:
+            return user.modified_count > 0
+    except Exception as e:
+        print(e)
+        return False
+
+def delete_user(user_id):
+    try:
+        user = collection.delete_one({"_id": user_id})
+        return user.deleted_count > 0
+    except Exception as e:
+        print(e)
+        return False
+
+# user_schema = {
+#     "validator": {
+#         "$jsonSchema": {
+#             "bsonType": "object",
+#             "required": ["firstName", "lastName", "username", "password", "userID", "studySets"],
+#             "properties": {
+#                 "firstName": {"bsonType": "string"},
+#                 "lastName": {"bsonType": "string"},
+#                 "username": {"bsonType": "string"},
+#                 "password": {"bsonType": "string"},
+#                 "userID": {"bsonType": "string"},
+#                 "studySets": {
+#                     "bsonType": "array",
+#                     "items": {"bsonType": "string"}
+#                 }
+#             }
+#         }
+#     }
+# }
