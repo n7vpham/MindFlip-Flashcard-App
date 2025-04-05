1dbf370eb77b8a29f95cca5ff8285c0782889170
isreednc <isaacreednc99@gmail.com>
2025-04-05 17:08:37 -0400

    Initial md file conversion function

---
+++ app/utils/conversions.py
@@ -0,0 +1,29 @@
+import re
+
+class FileConvert:
+    def md_to_flashcard_list(md_file):
+        flashcard_list = []
+        with open(md_file, 'r') as file:
+            prevLine = file.readline()
+            for line in file:
+                if re.search("^:", line):
+                    front = prevLine
+                    back = line
+                    for next_line in file:
+                        prevLine = next_line
+                        if next_line.strip() == "":
+                            break
+                        back += next_line
+                    flashcard_list.append({
+                        "front": front,
+                        "back": back
+                    })
+                else:
+                    prevLine = line
+        
+        return flashcard_list
+                    
+
+
+if __name__ == '__main__':
+    print(FileConvert.md_to_flashcard_list('testmd.md'))
---
+++ app/utils/testmd.md
@@ -0,0 +1,11 @@
+# This is a test
+
+Test def
+: Lest see how this looks
+
+Yes yes
+: we shall see how this appears
+- aa
+- aa
+
+how does the string appear.
